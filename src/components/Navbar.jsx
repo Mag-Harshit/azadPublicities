ba9b8081b0d95c31272999af84e2352e51df4f33
ninfu321 <harshit@Harshits-MacBook-Pro.local>
2024-06-25 18:42:52 -0700

    color changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,13 +10,16 @@ const Navbar = () => {
   }
 
   return (
-        <nav>
+        <nav className='border-black border-b-1 shadow'>
       <div className='flex flex-col justify-center items-center lg:justify-between lg:items-center lg:flex-row'>
       <div className='flex flex-col sm:flex-row justify-between items-center w-full lg:w-auto'>
       <div>
+      <a href='/'>
         <img id="logo" src="/img/Azad-Logo.png" alt="Azad Logo" width={"500px"}/>
+        </a>
         </div>
-        <div className='text-white flex items-center flex-col lg:flex-row'>
+        
+        <div className='text-black flex items-center flex-col lg:flex-row'>
         <button className='cursor-pointer box-content lg:hidden w-12 m-4 sm:m-0' onClick={handleVisibility} id='menu'>
         <svg xmlns="http://www.w3.org/2000/svg" className="w-12" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
              <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
@@ -25,7 +28,7 @@ const Navbar = () => {
         </div>
             
         </div>
-        <ul className={`text-white ${itemVisible ? 'flex' : 'hidden'} flex-col items-center lg:flex lg:flex-row`} id="items">
+        <ul className={`text-black ${itemVisible ? 'flex' : 'hidden'} flex-col items-center lg:flex lg:flex-row`} id="items">
            <li> <button className='m-3 lg:block text-lg hover:text-gray-400'>Home</button></li>
            <li><button className='m-3 lg:block text-lg hover:text-gray-400'>About Us</button></li>
            <li> <button className='m-3 lg:block text-lg hover:text-gray-400'>Services</button></li>
