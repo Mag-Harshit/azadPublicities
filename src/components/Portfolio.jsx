eceb91bbb9096ae03ba715122b59b7da375b90e4
ninfu321 <harshit@Harshits-MacBook-Pro.local>
2024-07-08 20:57:57 -0700

    Services Changes

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const Portfolio = () => {
+  useEffect(() => {
+    const handleScroll = () => {
+      const text = document.querySelectorAll(".transiText");
+      text.forEach((texts) => {
+        const rect = texts.getBoundingClientRect();
+        if (rect.top < window.innerHeight) {
+          texts.classList.add("visible");
+        }
+      });
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    handleScroll();
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
   return (
-    <div>
-      <div className="PortfolioMain">
-        <h1 className="text-7xl font-extrabold text-center pt-5">Portfolio</h1>
-        <h2 className="text-5xl font-semibold text-center mt-4">
+    <div className="PortfolioMain">
+      <div>
+        <h1 className="text-7xl font-extrabold text-center p-5 transiText">
+          Portfolio
+        </h1>
+        <h2 className="text-5xl font-semibold text-center mt-5 transiText">
           Showcasing Our Creative and Impactful Advertising Solutions
         </h2>
+        <p className="mt-4 ml-2 text-2xl px-1 pb-8 transiText">
+          "Welcome to our portfolio. Here, we present a selection of our work
+          that highlights the diverse and impactful advertising solutions we've
+          crafted for our clients. Our commitment to excellence ensures that
+          each project we undertake achieves remarkable results and leaves a
+          lasting impression."
+        </p>
       </div>
     </div>
   );
--- src/components/ServicesMain.jsx
+++ src/components/ServicesMain.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const ServicesMain = () => {
   return (
-    <div>
-      <div className="servicesTop">
+    <div className="servicesTop">
+      <div>
         <div className="text-7xl font-extrabold text-center p-8">
           <h1>SERVICES</h1>
         </div>
@@ -15,203 +15,143 @@ const ServicesMain = () => {
           </p>
         </div>
       </div>
-      <hr />
-      <div className="flex flex-col pb-8">
+      <div className="flex flex-row flex-wrap justify-center gap-10 pb-8">
         <div className="servicesCard border-3">
           <img
             src="/img/Cinema_Services.jpg"
-            className="mx-auto w-72 lg:ml-5 lg:w-1/4"
+            className="mx-auto w-full h-50 lg:ml-5 imgServices "
           />
           <div className="flex flex-col">
-            <h2 className="text-4xl font-semibold mb-6 text-center lg:text-left">
+            <h2 className="text-2xl font-semibold mb-6 text-center lg:text-left">
               Cinema Advertising
             </h2>
-            <p className="text-lg md:text-2xl text-center lg:text-left">
-              Cinema advertising is a powerful tool that Azad Publicities uses
-              to engage audiences in an immersive environment. This service
-              involves creating high-impact visual and audio advertisements that
-              are displayed before movies in local cinemas. The ads are designed
-              to captivate the audience's attention and leave a lasting
-              impression. By leveraging the captive audience in a relaxed and
-              attentive state, cinema advertising ensures that brand messages
-              are effectively communicated. This type of advertising is
-              particularly effective for promoting new products, local
-              businesses, and events, as it reaches a diverse and engaged
-              audience.
+            <p className=" text-sm md:text-lg text-center lg:text-left">
+              Engage audiences with high-impact ads shown before movies in local
+              cinemas. Perfect for promoting new products, businesses, and
+              events.
             </p>
           </div>
         </div>
         <div className="servicesCard border-3">
           <img
             src="/img/Radio_Services.jpg"
-            className="mx-auto w-72 lg:ml-5 lg:w-1/4"
+            className="mx-auto w-full h-50 lg:ml-5 imgServices"
           />
           <div className="flex flex-col">
-            <h2 className="text-4xl font-semibold mb-6 text-center lg:text-left">
-              Radio Commercial Advertising
+            <h2 className="text-2xl font-semibold mb-6 text-center lg:text-left">
+              Radio Advertising
             </h2>
-            <p className="text-lg md:text-2xl text-center lg:text-left">
-              Radio advertising remains a relevant and effective medium for
-              reaching a broad audience. Azad Publicities produces and
-              broadcasts compelling radio commercials that convey brand messages
-              through engaging storytelling and catchy jingles. Radio ads are
-              crafted to resonate with listeners during their daily routines,
-              whether they are commuting, working, or relaxing at home. This
-              service takes advantage of the wide reach of radio stations and
-              the intimate nature of the audio medium to build strong brand
-              awareness and influence consumer behavior.
+            <p className=" text-sm md:text-lg text-center lg:text-left">
+              Reach a broad audience with compelling radio ads featuring
+              engaging storytelling and catchy jingles. Ideal for building brand
+              awareness.
             </p>
           </div>
         </div>
         <div className="servicesCard border-3">
           <img
             src="/img/Unipole_Services.jpg"
-            className="mx-auto w-72 lg:ml-5 lg:w-1/4"
+            className="mx-auto w-full h-50 lg:ml-5 imgServices"
           />
           <div className="flex flex-col">
-            <h2 className="text-4xl font-semibold mb-6 text-center lg:text-left">
+            <h2 className="text-2xl font-semibold mb-6 text-center lg:text-left">
               Unipole Advertising
             </h2>
-            <p className="text-lg md:text-2xl text-center lg:text-left">
-              Unipole advertising involves the installation of large-scale
-              advertisements on tall, single poles strategically placed in
-              high-traffic areas. Azad Publicities specializes in creating
-              visually striking unipole ads that are impossible to miss. These
-              ads are ideal for promoting products, services, and events to a
-              broad audience. By positioning unipole advertisements along busy
-              roads and highways, Azad Publicities ensures maximum visibility
-              and impact. This type of advertising is particularly effective for
-              reaching motorists and pedestrians in urban and suburban areas.
+            <p className=" text-sm md:text-lg text-center lg:text-left">
+              Large-scale ads on tall poles in high-traffic areas, ensuring
+              maximum visibility. Great for promoting products and events.
             </p>
           </div>
         </div>
         <div className="servicesCard border-3">
           <img
             src="/img/Neon_Services.jpg"
-            className="mx-auto w-72 lg:ml-5 lg:w-1/4"
+            className="mx-auto w-full h-50 lg:ml-5 imgServices"
           />
           <div className="flex flex-col">
-            <h2 className="text-4xl font-semibold mb-6 text-center lg:text-left">
+            <h2 className="text-2xl font-semibold mb-6 text-center lg:text-left">
               Neon Signs
             </h2>
-            <p className="text-lg md:text-2xl text-center lg:text-left">
-              Neon signs are a classic and effective way to draw attention to
-              businesses, especially at night. Azad Publicities designs and
-              installs custom neon signs that feature bright, vibrant colors and
-              eye-catching designs. These signs are perfect for storefronts,
-              restaurants, bars, and entertainment venues. Neon signs not only
-              enhance the visibility of a business but also contribute to its
-              branding and aesthetic appeal. By using high-quality materials and
-              innovative designs, Azad Publicities helps businesses stand out in
-              crowded markets and attract more customers.
+            <p className=" text-sm md:text-lg text-center lg:text-left">
+              Custom neon signs that enhance business visibility and branding,
+              especially at night. Ideal for storefronts and entertainment
+              venues.
             </p>
           </div>
         </div>
         <div className="servicesCard border-3">
           <img
             src="/img/Boards_Services.jpg"
-            className="mx-auto w-72 lg:ml-5 lg:w-1/4"
+            className="mx-auto w-full h-50 lg:ml-5 imgServices"
           />
           <div className="flex flex-col">
-            <h2 className="text-4xl font-semibold mb-6 text-center lg:text-left">
+            <h2 className="text-2xl font-semibold mb-6 text-center lg:text-left">
               Boards and Banners
             </h2>
-            <p className="text-lg md:text-2xl text-center lg:text-left">
-              Boards and banners are versatile advertising tools that can be
-              used in a variety of settings. Azad Publicities offers custom
-              design and installation of boards and banners for events,
-              promotions, and business advertising. These can range from small,
-              portable banners to large, fixed boards. By utilizing durable
-              materials and high-resolution printing, Azad Publicities ensures
-              that the advertisements are both attractive and long-lasting.
-              Boards and banners are ideal for conveying messages in retail
-              environments, trade shows, outdoor events, and more.
+            <p className=" text-sm md:text-lg text-center lg:text-left">
+              Versatile advertising tools for events, promotions, and business
+              advertising. Durable and attractive for various settings.
             </p>
           </div>
         </div>
         <div className="servicesCard border-3">
           <img
             src="/img/Kiosks_Services.jpg"
-            className="mx-auto w-72 lg:ml-5 lg:w-1/4"
+            className="mx-auto w-full h-3/6 lg:ml-5"
           />
           <div className="flex flex-col">
-            <h2 className="text-4xl font-semibold mb-6 text-center lg:text-left">
+            <h2 className="text-2xl font-semibold mb-6 text-center lg:text-left">
               Kiosks
             </h2>
-            <p className="text-lg md:text-2xl text-center lg:text-left">
-              Kiosks provide an interactive and direct way for businesses to
-              engage with customers. Azad Publicities designs and sets up kiosks
-              at strategic locations such as malls, trade shows, and public
-              events. These kiosks can be used for product demonstrations,
-              information distribution, and customer interactions. By providing
-              a physical presence in high-traffic areas, kiosks help businesses
-              create personal connections with potential customers, gather
-              feedback, and drive sales. Azad Publicities ensures that each
-              kiosk is visually appealing and functionally effective, tailored
-              to the specific needs of the client.
+            <p className=" text-sm md:text-lg text-center lg:text-left">
+              Interactive kiosks for engaging customers at malls, trade shows,
+              and public events. Perfect for product demos and information
+              distribution.
             </p>
           </div>
         </div>
         <div className="servicesCard border-3">
           <img
             src="/img/Hoardings_Services.jpg"
-            className="mx-auto w-72 lg:ml-5 lg:w-1/4"
+            className="mx-auto w-full h-50 lg:ml-5 imgServices"
           />
           <div className="flex flex-col">
-            <h2 className="text-4xl font-semibold mb-6 text-center lg:text-left">
+            <h2 className="text-2xl font-semibold mb-6 text-center lg:text-left">
               Hoardings
             </h2>
-            <p className="text-lg md:text-2xl text-center lg:text-left">
-              Hoarding advertising involves placing large billboards in prime
-              locations to maximize exposure. Azad Publicities excels in
-              creating impactful hoarding advertisements that capture the
-              attention of passersby. These hoardings are strategically placed
-              along busy roads, highways, and city centers, ensuring that they
-              reach a wide audience. By using high-quality graphics and
-              compelling messages, Azad Publicities helps brands make a strong
-              visual statement and reinforce their presence in the market.
+            <p className=" text-sm md:text-lg text-center lg:text-left">
+              Large billboards in prime locations to maximize exposure. Designed
+              to capture attention and reinforce brand presence.
             </p>
           </div>
         </div>
         <div className="servicesCard border-3">
           <img
             src="/img/Bus_Services.jpg"
-            className="mx-auto w-72 lg:ml-5 lg:w-1/4"
+            className="mx-auto w-full h-50 lg:ml-5 imgServices"
           />
           <div className="flex flex-col">
-            <h2 className="text-4xl font-semibold mb-6 text-center lg:text-left">
+            <h2 className="text-2xl font-semibold mb-6 text-center lg:text-left">
               Bus Shelters
             </h2>
-            <p className="text-lg md:text-2xl text-center lg:text-left">
-              Advertising on bus shelters is a highly effective way to reach
-              daily commuters. Azad Publicities leverages this medium to display
-              advertisements that are seen by thousands of people every day.
-              These ads are placed at bus shelters located in busy urban areas,
-              providing high visibility to both pedestrians and motorists. Bus
-              shelter advertising is an excellent choice for local businesses
-              and public service announcements, as it targets a consistent flow
-              of daily commuters who spend time waiting at these shelters.
+            <p className=" text-sm md:text-lg text-center lg:text-left">
+              High-visibility ads at bus shelters, targeting daily commuters.
+              Ideal for local businesses and public announcements.
             </p>
           </div>
         </div>
         <div className="servicesCard border-3">
           <img
             src="/img/Railway_Services.jpg"
-            className="mx-auto w-72 lg:ml-5 lg:w-1/4"
+            className="mx-auto w-full h-50 lg:ml-5 imgServices"
           />
           <div className="flex flex-col">
-            <h2 className="text-4xl font-semibold mb-6 text-center lg:text-left">
+            <h2 className="text-2xl font-semibold mb-6 text-center lg:text-left">
               Railway Advertising
             </h2>
-            <p className="text-lg md:text-2xl text-center lg:text-left">
-              Railway advertising involves placing ads in railway stations and
-              on trains, reaching a diverse and captive audience. Azad
-              Publicities designs and implements advertisements that effectively
-              communicate brand messages to travelers and daily commuters. This
-              service includes a variety of formats, such as posters, digital
-              screens, and train wraps. By advertising in railway environments,
-              businesses can achieve high visibility and frequency, ensuring
-              their messages are seen multiple times by a large audience.
+            <p className=" text-sm md:text-lg text-center lg:text-left">
+              Ads in railway stations and on trains, reaching a diverse audience
+              of travelers and commuters. Various formats available.
             </p>
           </div>
         </div>
